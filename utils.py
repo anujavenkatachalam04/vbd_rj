ea06d28a5e4102f6c65f09649547c139efe84bb3
21f3001926 <anuja@khushibaby.org>
2025-07-22 18:06:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -26,12 +26,21 @@ def load_drive(gdrive_secrets):
 # --- Sort district names ---
 def get_sorted_districts(df):
     individual_districts = [d for d in df['dtname_disp'].unique() if d.lower() != 'all']
+
     def sort_key(name):
-        match = re.search(r'\(High District - (\d+)\)', name)
-        if match:
-            return (0, int(match.group(1)))
-        else:
+        # High Districts
+        match_high = re.search(r'\(High District - (\d+)\)', name)
+        if match_high:
+            return (0, int(match_high.group(1)))
+
+        # Districts with High Blocks
+        elif "(Has High Blocks)" in name:
             return (1, name.lower())
+
+        # Other districts
+        else:
+            return (2, name.lower())
+
     sorted_districts = sorted(individual_districts, key=sort_key)
     return ['All'] + sorted_districts
 
