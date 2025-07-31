38a9f19ce4602bec4e4a295375e3234640303130
21f3001926 <anuja@khushibaby.org>
2025-07-31 19:54:31 +0530

    Update home.py

--- home.py
+++ home.py
@@ -11,7 +11,7 @@ Welcome to the Dengue-Climate Dashboard for Rajasthan!
 ### Navigation:
 Use the sidebar to explore the following pages:
 
-- **Weekly Trends** (2024 Dengue Season - Jul-Dec) : Weekly Time Series charts for each block within a district.
+- **Weekly Trends** (2024 Dengue Season - Jul-Dec) : Weekly Time Series charts for each block within a district. The top 10% of districts/blocks reporting dengue cases are labeled as 'High'.
 - **Monthly Trends** (2022-2024): Monthly Time Series charts for each block within a district.
 - **Top Blocks Trends** (2024 Dengue Season - Jul-Dec): Weekly Time Series charts for blocks with the highest blocks across the state.
 - **Breeding Conditions** (2024 Jan-Dec): Shows how ideal breeding conditions (using the specified threshold) map to cases by week and block.
